2c6fba75d124a9e99c46c9495a9d37fcc0bd8abf
Jake M <jmcgillicuddy@ewu.edu>
2023-04-23 19:23:35 -0700

    Refactored Object Library to use an ItemList node

--- assets/scenes/library_item.gd
+++
@@ -1,6 +0,0 @@
-class_name LibraryItem
-extends Control
-
-
-func set_data(name: String):
-	$"VBoxContainer/ItemName".text = name
--- assets/scenes/object_library.gd
+++ assets/scenes/object_library.gd
@@ -1,11 +1,22 @@
-extends GridContainer
+extends Control
 
-@onready var library_item: PackedScene = preload("res://assets/scenes/library_item.tscn")
+
+@onready var object_list: ItemList = get_node("Object List")
 
 func _ready():
+	populate_library("")
+	pass
+
+
+func populate_library(filter: String): 
 	var keys = JsonParser.garden_data.keys()
-	print(keys)
 	for s in keys:
-		var item: LibraryItem = library_item.instantiate()
-		item.set_data(s)
-		add_child(item)
+		if filter.is_empty():
+			object_list.add_item(s)
+		if filter in s.to_lower():
+			object_list.add_item(s)
+
+
+func _on_object_filter_text_changed(new_text):
+	object_list.clear()
+	populate_library(new_text)
