0fb4defc38e737ac7f4a7331af4ac40bc943236e
Jessabel Donner <jdoner+hellothisisaflag@ewu.edu>
2023-04-25 15:21:59 -0700

    adds scripts to dialogue box and custom signal for garden creation

--- assets/scripts/controller.gd
+++ assets/scripts/controller.gd
@@ -1,9 +1,11 @@
 extends Node
 var _garden_scene = preload("res://assets/scenes/Garden.tscn")
+var _garden_creation_popup_scene = preload("res://assets/scenes/garden_creation_popup.tscn")
 var _garden
 var _garden_plan:GardenPlan
 enum File_Menu_Options {
-	EXIT = 1
+	EXIT = 1,
+	CREATE_GARDEN = 2,
 }
 ##[method _on_file_id_pressed]:
 ##Connected to the file menu's [signal PopupMenu.index_pressed]
@@ -12,6 +14,10 @@ func _on_file_id_pressed(id):
 		File_Menu_Options.EXIT:
 			get_tree().get_root().propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 			get_tree().quit()
+		#File_Menu_Options.CREATE_GARDEN:
+			#var garden_creation_popup = _garden_creation_popup_scene.instantiate()
+			#self.add_child(garden_creation_popup)
+			
 		_:
 			push_warning("Menu Item not found")
 ##[method create_garden]:
