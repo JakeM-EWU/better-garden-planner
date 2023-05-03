b75f2cafc297d47dde0ab84a4b770da224a50720
Jake M <jmcgillicuddy@ewu.edu>
2023-05-03 14:17:59 -0700

    Refactored garden_plan and created enum for layers

--- assets/scripts/garden_plan.gd
+++ assets/scripts/garden_plan.gd
@@ -9,11 +9,6 @@ extends TileMap
 var _placeable_tile_source_id = 0
 
 var _currently_selected_source_id = 1
-#The layer where background tiles are placed
-var _garden_layer_id = 0
-#The layer where garden objects are placed
-var _object_layer_id = 1
-
 
 ##The height of the garden in pixels
 var garden_height_in_pixels: int:
@@ -24,9 +19,20 @@ var garden_width_in_pixels: int:
 	get:
 		return columns * cell_quadrant_size
 
+enum Layer {
+	GARDEN = 0,
+	OBJECT = 1,
+}
 
 func _ready():
 	_init_garden()
+
+
+##[method _init_garden]:
+##Creates the tiles for the garden and centers it.
+func _init_garden():
+	_generate_tiles()
+	_center_garden()
 
 
 #for testing purposes
@@ -39,11 +45,31 @@ func _process(delta):
 func set_source_id(id: int):
 	_currently_selected_source_id = id
 
+##[method place_object]:
+##Attempts to place an object at the location [param tile].
+##The sprite of the object is selected using [param tilemap_spritesheet_id] and [param sprite_coords].
+##Returns [code]true[/code] if an object was placed or [code]false[/code] if not.
+func place_object(tile: Vector2i, tilemap_spritesheet_id: int, sprite_coords: Vector2)->bool:
+		var out = tile_is_placeable(tile)
+		if 	out:
+			set_cell(Layer.OBJECT, tile, tilemap_spritesheet_id, sprite_coords)
+		return out
+
+
+##[method remove_object]:
+##Attempts to remove an object at the location [param tile].
+##Returns [code]true[/code] if an object was removed or [code]false[/code] if not.
+func remove_object(tile: Vector2i)->bool:
+		var out = !tile_is_empty(tile)
+		if 	out:
+			set_cell(Layer.OBJECT, tile, -1)
+		return out
+
 
 ##[method tile_is_placeable]:
 ##Returns true if the location [param tile] can have an object placed in it.
 func tile_is_placeable(tile: Vector2i) -> bool:
-	var garden_id = get_cell_source_id(_garden_layer_id, tile)
+	var garden_id = get_cell_source_id(Layer.GARDEN, tile)
 	
 	if garden_id == _placeable_tile_source_id && tile_is_empty(tile):
 		return true
@@ -53,16 +79,9 @@ func tile_is_placeable(tile: Vector2i) -> bool:
 ##[method tile_is_empty]:
 ##Returns true if the location [param tile] doesn't have an object.
 func tile_is_empty(tile: Vector2i) -> bool:
-	var object_id = get_cell_source_id(_object_layer_id, tile)
+	var object_id = get_cell_source_id(Layer.OBJECT, tile)
 
 	return object_id == -1
-
-
-##[method _init_garden]:
-##Creates the tiles for the garden and centers it.
-func _init_garden():
-	_generate_tiles()
-	_center_garden()
 
 
 ##[method _generate_tiles]:
@@ -70,7 +89,7 @@ func _init_garden():
 func _generate_tiles():
 	for r in rows:
 		for c in columns:
-			set_cell(_garden_layer_id, Vector2i(c, r), _placeable_tile_source_id, Vector2i(0,0))
+			set_cell(Layer.GARDEN, Vector2i(c, r), _placeable_tile_source_id, Vector2i(0,0))
 
 
 ##[method center_garden]:
@@ -81,24 +100,3 @@ func _center_garden():
 	
 	self.position.x = x_pos
 	self.position.y = y_pos
-
-
-##[method place_object]:
-##Attempts to place an object at the location [param tile].
-##The sprite of the object is selected using [param tilemap_spritesheet_id] and [param sprite_coords].
-##Returns [code]true[/code] if an object was placed or [code]false[/code] if not.
-func place_object(tile: Vector2i, tilemap_spritesheet_id: int, sprite_coords: Vector2)->bool:
-		var out = tile_is_placeable(tile)
-		if 	out:
-			set_cell(_object_layer_id, tile, tilemap_spritesheet_id, sprite_coords)
-		return out
-
-
-##[method remove_object]:
-##Attempts to remove an object at the location [param tile].
-##Returns [code]true[/code] if an object was removed or [code]false[/code] if not.
-func remove_object(tile: Vector2i)->bool:
-		var out = !tile_is_empty(tile)
-		if 	out:
-			set_cell(_object_layer_id, tile, -1)
-		return out
