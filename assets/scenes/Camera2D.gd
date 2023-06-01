a0f56bc9217c7f1eb0bf42517ae9cdc589723f97
Jake M <jmcgillicuddy@ewu.edu>
2023-05-31 19:26:21 -0700

    Added bounds to camera panning

--- assets/scenes/Camera2D.gd
+++ assets/scenes/Camera2D.gd
@@ -1,21 +1,35 @@
 extends Camera2D
 
-const MIN_ZOOM: float = 0.1
-const MAX_ZOOM: float = 1.0
-const ZOOM_INCREMENT: float = 0.1
+const MinZoom: float = 0.1
+const MaxZoom: float = 1.5
+const ZoomIncrement: float = 0.1
 
 var _current_zoom: float = 1.0
+var x_bounds
+var y_bounds
+
+func _ready():
+	GardenSignalBus.size_set.connect(_on_size_set)
+	pass
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		if event.button_mask == MOUSE_BUTTON_MASK_RIGHT:
-			position -= event.relative / _current_zoom
+			var new_position = position - (event.relative / _current_zoom)
+			var x = clampf(new_position.x, -x_bounds, x_bounds)
+			var y = clampf(new_position.y, -y_bounds, y_bounds)
+			position = Vector2(x, y)
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				_current_zoom = max(_current_zoom - ZOOM_INCREMENT, MIN_ZOOM)
+				_current_zoom = max(_current_zoom - ZoomIncrement, MinZoom)
 				self.zoom = _current_zoom * Vector2.ONE
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				_current_zoom = max(_current_zoom + ZOOM_INCREMENT, MAX_ZOOM)
+				_current_zoom = min(_current_zoom + ZoomIncrement, MaxZoom)
 				self.zoom = _current_zoom * Vector2.ONE
+		print(zoom)
 
+
+func _on_size_set(rows: int, columns: int):
+	x_bounds = (columns / 2) * 32	
+	y_bounds = (rows / 2) * 32
